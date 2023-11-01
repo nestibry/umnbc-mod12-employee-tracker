50b2ab814230d5d77698b2210ed5b196bc23d47b
nestibry <nestibry@gmail.com>
2023-11-01 08:51:58 -0500

    added all choices, including bonus actions, to the action prompt

--- index.js
+++ index.js
@@ -5,26 +5,40 @@ const questions = [
     type: 'list',
     name: 'action',
     message: 'Select an action:',
-    choices: ['Option 1', 'Option 2', 'Quit'],
+    choices: [
+        'View All Departments', 
+        'View All Employees',
+        'View All Roles', 
+        'Add a Department',
+        'Add an Employee',
+        'Add a Role',
+        'Update an Employee Role',
+        'Update Employee Managers',   // Bonus
+        'View Employees by Manager',  // Bonus
+        'View Employees by Department', //Bonus
+        'Delete Departments, Roles, and Employees', // Bonus
+        'View Utilized Budget of a Department',  // Bonus: i.e., the combined salaries of all employees in that department
+        'Exit Employee Manager'],
   },
 ];
 
 async function main() {
   while (true) {
+    console.log('Main Menu');
     const answers = await inquirer.prompt(questions);
 
-    if (answers.action === 'Quit') {
+    if (answers.action === 'Exit Employee Manager') {
       console.log('Goodbye!');
-      break; // Exit the loop when the user selects "Quit"
+      break; // Exit the loop when the user selects "Exit Employee Manager"
     }
 
     // Handle other options here based on answers.action
     switch (answers.action) {
-      case 'Option 1':
-        console.log('You selected Option 1');
+      case 'View All Departments':
+        console.log('Viewing All Departments:');
         break;
-      case 'Option 2':
-        console.log('You selected Option 2');
+      case 'View All Roles':
+        console.log('Viewing All Roles: ');
         break;
     }
   }
