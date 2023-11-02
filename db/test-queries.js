380f0c510827fb6e6994463491b429e4a745ab33
nestibry <nestibry@gmail.com>
2023-11-02 15:15:39 -0500

    updated test-queries.sql with correct table names

--- db/test-queries.js
+++ db/test-queries.js
@@ -5,21 +5,21 @@ db;
 console.log("Starting Queries...");
 
 // // View all Departments
-// db.query(`SELECT * FROM department`, function (err, results) {
+// db.query(`SELECT * FROM departments`, function (err, results) {
 //     console.log("View All Departments");
 //     console.table(results);
 // });
 
 
 // // View All Roles (w/o department)
-// db.query(`SELECT * FROM role`, function (err, results) {
+// db.query(`SELECT * FROM roles`, function (err, results) {
 //     console.log("View All Roles (w/o department)");
 //     console.table(results);
 // });
 
 
 // // View All Employees (w/o role and manager)
-// db.query(`SELECT * FROM employee`, function (err, results) {
+// db.query(`SELECT * FROM employees`, function (err, results) {
 //     console.log("View All Employees (w/o role and manager)");
 //     console.table(results);
 // });
@@ -28,20 +28,20 @@ console.log("Starting Queries...");
 // // View All Roles
 // db.query(`
 //     SELECT r.id, r.title, d.name as department, r.salary
-//     FROM department d
-//     INNER JOIN role r ON d.id = r.department_id`,
+//     FROM departments d
+//     INNER JOIN roles r ON d.id = r.department_id`,
 //     function (err, results) {
 //         console.log("View All Roles");
 //         console.table(results);
 // });
 
 
-// View All Employees
+// // View All Employees
 // db.query(`
-//     SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, concat(m.first_name, ' ' ,m.last_name) AS manager FROM employee e
-//     INNER JOIN role r ON e.role_id = r.id
-//     INNER JOIN department d ON r.department_id = d.id
-//     LEFT JOIN employee m ON e.manager_id = m.id`,
+//     SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, concat(m.first_name, ' ' ,m.last_name) AS manager FROM employees e
+//     INNER JOIN roles r ON e.role_id = r.id
+//     INNER JOIN departments d ON r.department_id = d.id
+//     LEFT JOIN employees m ON e.manager_id = m.id`,
 //     function (err, results) {
 //         console.log("View All Employees");
 //         console.table(results);
