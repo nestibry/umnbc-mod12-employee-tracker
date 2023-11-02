1d4bed20cea7abd2cc2fc93f6bed90632fc20e86
nestibry <nestibry@gmail.com>
2023-11-02 15:10:48 -0500

    updated queries.sql with correct table names

--- db/queries.sql
+++ db/queries.sql
@@ -1,18 +1,18 @@
 USE employee_db;
 
-SELECT * FROM department;
+SELECT * FROM departments;
 
-SELECT * FROM role;
+SELECT * FROM roles;
 
-SELECT * FROM employee;
+SELECT * FROM employees;
 
 -- View All Roles
 SELECT r.id, r.title, d.name as department, r.salary 
-FROM department d
-INNER JOIN role r ON d.id = r.department_id;
+FROM departments d
+INNER JOIN roles r ON d.id = r.department_id;
 
 -- View ALL Employees 
-SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, concat(m.first_name, ' ' ,m.last_name) AS manager FROM employee e
-INNER JOIN role r ON e.role_id = r.id
-INNER JOIN department d ON r.department_id = d.id
-LEFT JOIN employee m ON e.manager_id = m.id;
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, concat(m.first_name, ' ' ,m.last_name) AS manager FROM employees e
+INNER JOIN roles r ON e.role_id = r.id
+INNER JOIN departments d ON r.department_id = d.id
+LEFT JOIN employees m ON e.manager_id = m.id;
