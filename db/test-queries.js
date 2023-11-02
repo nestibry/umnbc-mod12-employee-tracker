55e2945acab686fb4f74f742b2e4c78a43245586
nestibry <nestibry@gmail.com>
2023-11-02 08:50:56 -0500

    console.table must already be included with node...i don't believe i needed to install any npm packages

--- db/test-queries.js
+++ db/test-queries.js
@@ -1,4 +1,5 @@
 const db = require('../config/connection.js');
+// const cTable = require('console.table');
 
 db;
 
@@ -6,15 +7,18 @@ console.log("Starting Queries...");
 
 db.query(`SELECT * FROM department`, function (err, results) {
     console.log("Departments:");
-    console.log(results);
+    // console.log(results);
+    console.table(results);
 });
 
 db.query(`SELECT * FROM role`, function (err, results) {
     console.log("Roles");
-    console.log(results);
+    // console.log(results);
+    console.table(results);
 });
 
 db.query(`SELECT * FROM employee`, function (err, results) {
     console.log("Employees");
-    console.log(results);
+    // console.log(results);
+    console.table(results);
 });
