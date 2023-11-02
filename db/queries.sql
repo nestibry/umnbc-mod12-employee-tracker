f77d9d2ce5ec1a68242c4c60a7bc4ddc5e7f259a
nestibry <nestibry@gmail.com>
2023-11-02 09:17:49 -0500

    init View All Roles query

--- db/queries.sql
+++ db/queries.sql
@@ -5,3 +5,7 @@ SELECT * FROM department;
 SELECT * FROM role;
 
 SELECT * FROM employee;
+
+SELECT r.id, r.title, d.name as department, r.salary 
+FROM department d
+INNER JOIN role r ON d.id = r.department_id;
--- db/test-queries.js
+++ db/test-queries.js
@@ -4,17 +4,44 @@ db;
 
 console.log("Starting Queries...");
 
+
 db.query(`SELECT * FROM department`, function (err, results) {
-    console.log("Departments:");
+    console.log("View All Departments");
     console.table(results);
 });
 
 db.query(`SELECT * FROM role`, function (err, results) {
-    console.log("Roles");
+    console.log("View All Roles (w/o department)");
     console.table(results);
 });
 
 db.query(`SELECT * FROM employee`, function (err, results) {
-    console.log("Employees");
+    console.log("View All Employees (w/o role and manager)");
     console.table(results);
 });
+
+
+db.query(`SELECT r.id, r.title, d.name as department, r.salary
+            FROM department d
+            INNER JOIN role r ON d.id = r.department_id`, 
+            function (err, results) {
+                console.log("View All Roles");
+                console.table(results);
+});
+
+
+/*
+    Queries remaining to execute:
+        View All Roles
+        'View All Employees',
+        'Add a Department',
+        'Add an Employee',
+        'Add a Role',
+        'Update an Employee Role',
+        // 'Update Employee Managers',   // Bonus
+        // 'View Employees by Manager',  // Bonus
+        // 'View Employees by Department', //Bonus
+        // 'Delete Departments, Roles, and Employees', // Bonus
+        // 'View Utilized Budget of a Department',  // Bonus: i.e., the combined salaries of all employees in that department
+*/
+
