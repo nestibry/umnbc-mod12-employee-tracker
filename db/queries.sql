6a86ba3037a525f7e73440fbde0ff176cb53cd94
nestibry <nestibry@gmail.com>
2023-11-02 14:48:52 -0500

    added view all employees

--- db/queries.sql
+++ db/queries.sql
@@ -6,6 +6,13 @@ SELECT * FROM role;
 
 SELECT * FROM employee;
 
+-- View All Roles
 SELECT r.id, r.title, d.name as department, r.salary 
 FROM department d
 INNER JOIN role r ON d.id = r.department_id;
+
+-- View ALL Employees 
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, concat(m.first_name, ' ' ,m.last_name) AS manager FROM employee e
+INNER JOIN role r ON e.role_id = r.id
+INNER JOIN department d ON r.department_id = d.id
+LEFT JOIN employee m ON e.manager_id = m.id;
