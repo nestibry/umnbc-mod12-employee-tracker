d25cdf123493373c89dee1b9640216bc33ac3bad
nestibry <nestibry@gmail.com>
2023-11-05 11:35:32 -0600

    updated connection.js and .env.EXAMPLE

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,16 @@
 const mysql = require('mysql2');
-// const mysql = require('mysql2/promise');
 require("dotenv").config();
 
 // Connect to MySQL database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
+        user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     },
     console.log(`Connected to the ${process.env.DB_NAME} database.`)
 );
 
-// db.connect((err) => err ? err : console.log(`Connected to the ${process.env.DB_NAME} database.`));
 
 module.exports = db;
