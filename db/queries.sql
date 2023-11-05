792077d2a4a40993590c0ee11452c4027776c647
nestibry <nestibry@gmail.com>
2023-11-05 10:52:03 -0600

    updated the Utilized Budget by Department query

--- db/queries.sql
+++ db/queries.sql
@@ -71,7 +71,9 @@ DELETE FROM employees WHERE id = 1;
 
 -- View Utilized Budget of a Department  
 -- Bonus: i.e., the combined salaries of all employees in that department
-SELECT d.name, SUM(r.salary) AS utilized_budget FROM roles r 
+SELECT d.name AS department, SUM(r.salary) AS utilized_budget FROM employees e
+INNER JOIN roles r ON e.role_id = r.id
 INNER JOIN departments d ON r.department_id = d.id
+LEFT JOIN employees m ON e.manager_id = m.id
 GROUP BY r.department_id;
 
