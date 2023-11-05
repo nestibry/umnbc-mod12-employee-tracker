1498bffa8ad95f7aeda0afccc66d183971c79499
nestibry <nestibry@gmail.com>
2023-11-05 08:56:06 -0600

    added to queries.sql => view managers, view employees by manager, view employees by department

--- db/queries.sql
+++ db/queries.sql
@@ -39,3 +39,23 @@ INNER JOIN departments d ON r.department_id = d.id
 LEFT JOIN employees m ON e.manager_id = m.id
 WHERE e.id = 2;
 
+-- View Managers
+SELECT e.manager_id, concat(m.first_name, ' ' ,m.last_name) AS manager_name FROM employees e
+INNER JOIN employees m ON e.manager_id = m.id;
+
+
+-- View Employees by Manager
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, concat(m.first_name, ' ' ,m.last_name) AS manager FROM employees e
+INNER JOIN roles r ON e.role_id = r.id
+INNER JOIN departments d ON r.department_id = d.id
+LEFT JOIN employees m ON e.manager_id = m.id
+WHERE e.manager_id = 1;
+
+
+-- View Employees by Department
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, concat(m.first_name, ' ' ,m.last_name) AS manager FROM employees e
+INNER JOIN roles r ON e.role_id = r.id
+INNER JOIN departments d ON r.department_id = d.id
+LEFT JOIN employees m ON e.manager_id = m.id
+WHERE r.department_id = 1;
+
